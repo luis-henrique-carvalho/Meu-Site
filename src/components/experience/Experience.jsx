1574c1ad50ad45dfa7de114dfc5a2b5fd9ca0435
innsanowr <luisdev9225@gmail.com>
2022-09-16 16:11:39 -0300

    Criação da pagina de experiencias

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,100 @@
-import React from 'react'
+import React from "react";
+import { BsPatchCheckFill } from "react-icons/bs";
+import "./experience.css";
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
-  )
-}
+    <section id="experience">
+      <h5>Quais Habilidades eu Tenho</h5>
+      <h2>Minha Experiência</h2>
 
-export default Experience
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Desenvolvedor Front-end</h3>
+          <div className="experience__content">
+            <article className="experience__detailes">
+              <BsPatchCheckFill />
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experiente</small>
+              </div>
+            </article>
+            <article className="experience__detailes">
+              <BsPatchCheckFill />
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Experiente</small>
+              </div>
+            </article>
+            <article className="experience__detailes">
+              <BsPatchCheckFill />
+              <div>
+                <h4>JAVA SCRIPT</h4>
+                <small className="text-light">Experiente</small>
+              </div>
+            </article>
+            <article className="experience__detailes">
+              <BsPatchCheckFill />
+              <div>
+                <h4>REACT.JS</h4>
+                <small className="text-light">Experiente</small>
+              </div>
+            </article>
+            <article className="experience__detailes">
+              <BsPatchCheckFill />
+              <div>
+                <h4>BOOTSTRAP</h4>
+                <small className="text-light">Estudando</small>
+              </div>
+            </article>
+          </div>
+        </div>
+
+        {/* DESIGNER GRAFICO EXPERIENCIAS */}
+        <div className="experience__designer">
+          <h3>Designer Gráfico</h3>
+
+          <div className="experience__content">
+            <article className="experience__detailes">
+              <BsPatchCheckFill />
+              <div>
+                <h4>PHOTOSHOP</h4>
+                <small className="text-light">Experiente</small>
+              </div>
+            </article>
+            <article className="experience__detailes">
+              <BsPatchCheckFill />
+              <div>
+                <h4>COREW DRAW</h4>
+                <small className="text-light">Experiente</small>
+              </div>
+            </article>
+            <article className="experience__detailes">
+              <BsPatchCheckFill />
+              <div>
+                <h4>ILLUSTRATOR</h4>
+                <small className="text-light">Experiente</small>
+              </div>
+            </article>
+            <article className="experience__detailes">
+              <BsPatchCheckFill />
+              <div>
+                <h4>AFTER EFECT</h4>
+                <small className="text-light">Experiente</small>
+              </div>
+            </article>
+            <article className="experience__detailes">
+              <BsPatchCheckFill />
+              <div>
+                <h4>FIGMA</h4>
+                <small className="text-light">Estudando</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Experience;
--- src/components/portifolio/Portifolio.jsx
+++ src/components/portifolio/Portifolio.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const Portifolio = () => {
   return (
-    <section id='portifolio'>Portifólio</section>
-  )
-}
+    <section id="portifolio">
+      <h5>Meu projetos</h5>
+      <h2>Portifólio</h2>
 
-export default Portifolio
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image"></div>
+          <h3>Este é o titulo do Portifólio</h3>
+          <a
+            href="https://github.com/luis-henrique-carvalho/Cust"
+            className="btn"
+          >
+            Github
+          </a>
+          <a
+            href="https://github.com/luis-henrique-carvalho/Cust"
+            className="btn btn-primary"
+          >
+            Github
+          </a>
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Portifolio;
